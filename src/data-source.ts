346e19aba39e2e7f67b119d736a164c4a69a4ae7
Avinash Chavan <avinashchavan1443@gmail.com>
2022-04-28 11:36:40 +0530

    remove extra lines

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,6 +1,5 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
-import { User } from "./entity/User";
 import { Ingredient } from "./entity/Ingredient";
 import { Beverage } from "./entity/Beverage";
 
